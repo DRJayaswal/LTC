b8f4131adf76ca3d1833806dd7c5451d184559cb
Dhruv Ratan Jayaswal <dhruvratanjayaswal@Dhruvs-Mac-mini.local>
2024-11-07 21:16:28 +0530

    new problem added

--- Can Construct.js
+++ Can Construct.js
@@ -1,18 +1,37 @@
-var canConstruct = function(ransomNote, magazine) {
-  if(ransomNote.length > magazine.length)
-    return false;
+var canConstruct = function (ransomNote, magazine) {
+  if (ransomNote.length > magazine.length) return false;
   var rA = ransomNote.split("");
   var mA = magazine.split("");
-  for (let i = 0; i < ransomNote.length; i++) {
-    for (let j = 0; j < magazine.length; j++) {
-
-      if(rA[i] == mA[j]){
-        rA[i] = null;
-        mA[j] = null;
-      }
+  const uC = [];
+  const rC = {};
+  const mC = {};
+  for (var i = 0; i < rA.length; i++){
+    if(!uC.includes(rA[i])){
+      uC.push(rA[i]);
     }
   }
-  return rA[rA.length - 1] == null;
+  for (let i = 0; i < rA.length; i++) {
+      if (rC[rA[i]] != undefined) {
+        rC[rA[i]]++;
+      } else {
+        rC[rA[i]] = 1;
+      }
+  }
+  for (let i = 0; i < mA.length; i++) {
+      if (mC[mA[i]] != undefined) {
+        mC[mA[i]]++;
+      } else {
+        mC[mA[i]] = 1;
+      }
+  }
+    for (let i = 0; i < uC.length; i++){
+      if(rC[uC[i]] <= mC[uC[i]]) {
+        continue
+      }else{
+        return false;
+      }
+    }
+    return true;
 };
 
-console.log(canConstruct("fihjjjjei","hjibagacbhadfaefdjaeaebgi"));
+console.log(canConstruct("fihjjjjei", "hjibagacbhadfaefdjaeaebgi"));
